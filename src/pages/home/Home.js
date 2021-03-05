a0197d7b4f15b174131ea784b3b5cc4f8764a566
Sandro Garcia <sndr95@hotmail.com>
2021-03-05 11:26:05 -0300

    logo shinoda

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,9 +1,6 @@
 import {Component} from 'react'
-import { MDBBtn } from 'mdbreact'
 
 import logo from '../../assets/images/shinoda.jpg'
-
-import Teste from './teste'
 
 export default class Home extends Component {
 
@@ -19,7 +16,7 @@ export default class Home extends Component {
 
         return (
             <div style = {{padding: "0em 1em 1em 1em", borderRadius: "10px", border: "2px solid", borderColor: "black", margin: "5em 1em 0 1em", textAlign: "center"}}>
-                {logo}
+                <img src="https://www.santaritaalarmes.com.br/img/clientes/original-a09cffce7fa6b36ba3a4bc6be033b9dc.jpg" alt="shinoda" />
              </div>
             
       )
