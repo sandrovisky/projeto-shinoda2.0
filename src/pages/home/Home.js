940b4ce14264cdee2178c1464f278d1c54fe9e65
Sandro Garcia <sndr95@hotmail.com>
2021-03-10 11:50:02 -0300

    Update Home.js

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -13,7 +13,8 @@ export default class Home extends Component {
     }
     
     render () {
-
+        window.sessionStorage.setItem("key", "chupa meu ovo");
+        alert(window.sessionStorage.getItem("key"))
         return (
             <div style = {{padding: "0em 1em 1em 1em", borderRadius: "10px", border: "2px solid", borderColor: "black", margin: "5em 1em 0 1em", textAlign: "center"}}>
                 <img style = {{width: "80%", height: "auto"}} src="https://www.santaritaalarmes.com.br/img/clientes/original-a09cffce7fa6b36ba3a4bc6be033b9dc.jpg" alt="shinoda" />
